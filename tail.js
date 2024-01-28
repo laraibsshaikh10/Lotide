a0a5a664fb8c477e897474af1ca1a778721f479e
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-01-28 11:31:49 -0500

    Updated a test case

--- tail.js
+++ tail.js
@@ -26,3 +26,9 @@ assertEqual(words.length, 3); // original array should still have 3 elements!
 const emptyArray = [];
 tail(emptyArray); // no need to capture the return value since we are not checking it
 assertEqual(emptyArray.length, 3); // original array should still have 3 elements!
+
+//Test case
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse");  //ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); //ensure second element is "Labs"
