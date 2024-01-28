45ff37215fd5e2d3d7b340f3c11cd82533c417be
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-01-27 21:50:57 -0500

    Updated head function

--- head.js
+++ head.js
@@ -2,9 +2,9 @@
 // const _ = require('lodash');
   const head = function(array){
     if (array.length !== 0) {
-      console.log (array[0]);
+      return array[0];
     } else {
-      console.log (undefined);
+      return undefined;
     }
   };
 
