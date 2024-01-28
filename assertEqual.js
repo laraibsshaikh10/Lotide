46e1d1e8ac521a258846b0b4e75b3aed7b309354
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-01-27 20:36:38 -0500

    Final code for console.assert function

---
+++ assertEqual.js
@@ -0,0 +1,37 @@
+// console.assert
+
+
+// FUNCTION IMPLEMENTATION
+// const sum = function(a, b) {
+//   return a + b;
+// }
+
+// // TEST CODE
+// console.assert(sum(1, 2) === 3);
+// console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+
+// FUNCTION IMPLEMENTATION
+// const sumBuggy = function(a, b) {
+//   return a * b;
+// }
+
+// // TEST CODE
+// console.assert(sumBuggy(1, 2) === 3); // fails, because bug!
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Laraib", "Shaikh");
+assertEqual("1", 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(4, 1);
