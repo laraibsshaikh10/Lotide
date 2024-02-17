773ae5375f56411cefef7419eabad46847ada0a1
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-02-17 04:49:40 -0500

    Refactored and modularized code for tail function along with the test file

--- tail.js
+++ tail.js
@@ -1,35 +1,9 @@
+
+
 //array will be sliced at index 1 to get the tail
 const tail = function(array) {
   return array.slice(1);
 };
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const oneWord = tail(["Lighthouse"]);
-assertEqual(oneWord.length, 0);
-assertEqual(oneWord[0], undefined);
-
-// Test Case: Check the original array
-const emptyArray = tail([]);
-assertEqual(emptyArray.length, 0);
-assertEqual(emptyArray[0], undefined);
-
-//Test case
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse");  //ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); //ensure second element is "Labs"
+module.exports = tail;
