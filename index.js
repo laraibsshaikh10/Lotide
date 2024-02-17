fc8819cc135745e6443de23ac07fc20a5a78de74
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-02-17 11:56:22 -0500

    Complete list of functions

--- index.js
+++ index.js
@@ -2,18 +2,43 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-
+const countLetters = require("./countLetters");
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
 
 //to import other functions present in the three files
 const assertEqual = require("./assertEqual");
 const assertArraysEqual = require("./assertArraysEqual");
 const eqArrays = require("./eqArrays");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const eqObjects = require("./eqObjects");
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
 
 module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
   assertEqual: assertEqual,
   assertArraysEqual: assertArraysEqual,
-  eqArrays: eqArrays
+  eqArrays: eqArrays,
+  assertObjectsEqual: assertObjectsEqual,
+  eqObjects: eqObjects,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+
 };
